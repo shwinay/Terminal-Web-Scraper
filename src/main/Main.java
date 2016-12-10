7b9c226b3a2bf9348d1684b75ed3caa7d9769357
Ashwin Kudva <winkudva@yahoo.com>
2016-12-09 22:34:30 -0600

    Finished preliminary time scrape feature

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,17 +1,43 @@
 package main;
 
+import java.util.Scanner;
+
+import web.ScrapeTime;
+
 public class Main 
 {
+	//VARIABLES
+	static ScrapeTime scrapeTime;
 	
+	
+	//METHODS
 	
 	public static void main(String[] args)
 	{
-		
+		init();
+		userInteraction();
 	}
 	
+	public static void init()
+	{
+		scrapeTime = new ScrapeTime();
+	}
 	
-	
-	
+	public static void userInteraction()
+	{
+		String input;
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("What would you like to find? For a list of commands, enter 'commands'");
+		input = scanner.nextLine();
+		
+		if (input.toLowerCase().equals("time"))
+		{
+			System.out.println("In which city would you like to know the time in?");
+			input = scanner.nextLine();
+			scrapeTime.scrapeTime(input);
+		}
+		
+	}
 	
 	
 }
--- src/web/ScrapeTime.java
+++ src/web/ScrapeTime.java
@@ -1,11 +1,57 @@
 package web;
 
+import java.io.IOException;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
 public class ScrapeTime 
 {
+
+	//VARIABLES
+	Document doc;
+	Document parser;
+	String time;
+	
+	//CONSTRUCTOR
+	public ScrapeTime()
+	{
+		
+	}
 	
 	
-	
-	
-	
+	//METHODS
+	public void scrapeTime(String searchQuery)
+	{
+		try 
+		{
+			doc = Jsoup.connect("https://www.timeanddate.com/worldclock/results.html?query=" + searchQuery).get();
+		} 
+		catch (IOException e) 
+		{
+			e.printStackTrace();
+		}
+		
+		try
+		{
+			Element timeElement = doc.getElementById("ct");
+			time = Jsoup.parse(timeElement.toString()).text();
+			
+			Elements locationElement = doc.select("h1");
+			String locationString = Jsoup.parse(locationElement.toString()).text();
+			
+			System.out.println(locationString + ": " + time);
+		}
+		catch(Exception e)
+		{
+			Element timeElement = doc.getElementById("p0");
+			time = Jsoup.parse(timeElement.toString()).text();
+			System.out.println(time);
+		}
+		
+		
+	}
 	
 }
